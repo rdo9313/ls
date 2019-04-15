8a1091c459bbbbd03046647e327a3c69171eca63
Ryan <rdo9313@gmail.com>
2019-04-15 13:23:18 -0400

    finalized car loan

--- lesson_2/car_loan_calculator/car_loan.rb
+++ lesson_2/car_loan_calculator/car_loan.rb
@@ -3,18 +3,74 @@ def prompt(message)
   puts "=> #{message}"
 end
 
+def valid_number?(num)
+  num.to_i != 0 && /\d/.match(num) && /^\d*\.?\d*$/.match(num)
+end
+
+def valid_integer?(integer)
+  integer.to_i != 0 && /^\d+$/.match(integer)
+end
+
 prompt("Welcome! I will help you figure out your car loan payments.")
 
-prompt("First, I will need to know your loan amount:")
-loan_amount = gets.chomp.to_f
+loop do
+  loan_amt = ""
+  prompt("First, I will need to know your loan amount:")
+  loop do
+    loan_amt = gets.chomp
 
-prompt("Next, I will need to know the APR of the loan (enter 3.2 for 3.2%):")
-apr = gets.chomp
-monthly_rate = apr.to_f / 1200
+    if valid_number?(loan_amt)
+      loan_amt = loan_amt.to_f
+      break
+    else
+      prompt("Not a valid loan amount. Try again:")
+    end
+  end
 
-prompt("Finally, I will need to know the loan duration in months:")
-loan_duration = gets.chomp.to_i
+  monthly_rate = ""
+  prompt("Next, I will need to know the APR of the loan (enter 3.2 for 3.2%):")
+  loop do
+    apr = gets.chomp
 
-monthly_payment = loan_amount * (monthly_rate / (1 - (1 + monthly_rate)**(-loan_duration)))
+    if valid_number?(apr)
+      monthly_rate = apr.to_f / 1200
+      break
+    else
+      prompt("Not a valid APR. Try again:")
+    end
+  end
 
-prompt("Your monthly payment is #{monthly_payment}")
+  loan_duration = ""
+  prompt("Finally, I will need to know the loan duration in months:")
+  loop do
+    loan_duration = gets.chomp
+
+    if valid_integer?(loan_duration)
+      loan_duration = loan_duration.to_i
+      break
+    else
+      prompt("Not a valid loan duration. Try again:")
+    end
+  end
+
+  form = (loan_amt * (monthly_rate / (1 - (1 + monthly_rate)**-loan_duration)))
+  monthly_payment = form.round(2)
+
+  prompt("Processing information...")
+  prompt("Your monthly payment will be $#{monthly_payment}")
+  prompt("Do you want to calculate again? (Y to repeat, N to quit)")
+
+  answer = gets.chomp
+  break if answer.downcase == "n"
+
+  valid_answer = ""
+  while answer.downcase != "y"
+    prompt("Enter Y to repeat, N to quit")
+    valid_answer = gets.chomp
+    break if valid_answer.downcase == "y" || valid_answer.downcase == "n"
+  end
+
+  break if valid_answer.downcase == "n"
+end
+prompt("----------------------------------------------------------------------")
+prompt("Thank you for using the car loan calculator. Hope you found it useful!")
