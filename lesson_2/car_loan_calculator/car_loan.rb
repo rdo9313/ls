c93a7d25416fc4452ec64d7a9ee725946302192e
Ryan <rdo9313@gmail.com>
2019-04-15 14:33:32 -0400

    refactored code

--- lesson_2/car_loan_calculator/car_loan.rb
+++ lesson_2/car_loan_calculator/car_loan.rb
@@ -3,74 +3,129 @@ def prompt(message)
   puts "=> #{message}"
 end
 
-def valid_number?(num)
-  num.to_i != 0 && /\d/.match(num) && /^\d*\.?\d*$/.match(num)
+def play_again?(answer)
+  if answer.downcase == "n"
+    return false
+  end
+
+  valid_answer = ""
+  while answer.downcase != "y" || answer.downcase != "n"
+    prompt("Enter Y to repeat, N to quit")
+    valid_answer = gets.chomp
+    break if valid_answer.downcase == "y" || valid_answer.downcase == "n"
+  end
+
+  if valid_answer.downcase == "n"
+    return false
+  else
+    return true
+  end
+
+  return true
 end
 
-def valid_integer?(integer)
+def valid_apr?(apr)
+  apr.to_f.to_s != "0.0" && /\d/.match(apr) && /^\d*\.?\d*$/.match(apr)
+end
+
+def valid_loan_amt?(loan_amt)
+  loan_amt.to_f.to_s != "0.0" && /\d/.match(loan_amt) && /^\d*\.?\d*$/.match(loan_amt)
+end
+
+def valid_duration?(integer)
   integer.to_i != 0 && /^\d+$/.match(integer)
 end
 
-prompt("Welcome! I will help you figure out your car loan payments.")
+def display_welcome
+  prompt("Welcome! I will help you figure out your car loan payments.")
+end
+
+def display_invalid_loan_amt
+  prompt("Not a valid loan amount. Try again:")
+end
+
+def display_invalid_apr
+  prompt("Not a valid APR. Try again:")
+end
+
+def display_invalid_duration
+  prompt("Not a valid loan duration. Try again:")
+end
+
+def request_loan_amt
+  prompt("First, I will need to know your loan amount:")
+end
+
+def request_apr
+  prompt("Next, I will need to know the APR of the loan (enter 3.2 for 3.2%):")
+end
+
+def request_duration
+  prompt("Finally, I will need to know the loan duration in months:")
+end
+
+def request_replay
+  prompt("Do you want to calculate again? (Y to repeat, N to quit)")
+end
+
+def display_monthly_payment(monthly_payment)
+  prompt("Processing information...")
+  prompt("Your monthly payment will be $#{monthly_payment}")
+end
+
+def display_end
+  prompt("----------------------------------------------------------------------")
+  prompt("Thank you for using the car loan calculator. Hope you found it useful!")
+end
+
+display_welcome
 
 loop do
   loan_amt = ""
-  prompt("First, I will need to know your loan amount:")
+  request_loan_amt
   loop do
     loan_amt = gets.chomp
 
-    if valid_number?(loan_amt)
+    if valid_loan_amt?(loan_amt)
       loan_amt = loan_amt.to_f
       break
     else
-      prompt("Not a valid loan amount. Try again:")
+      display_invalid_loan_amt
     end
   end
 
   monthly_rate = ""
-  prompt("Next, I will need to know the APR of the loan (enter 3.2 for 3.2%):")
+  request_apr
   loop do
     apr = gets.chomp
 
-    if valid_number?(apr)
+    if valid_apr?(apr)
       monthly_rate = apr.to_f / 1200
       break
     else
-      prompt("Not a valid APR. Try again:")
+      display_invalid_apr
     end
   end
 
   loan_duration = ""
-  prompt("Finally, I will need to know the loan duration in months:")
+  request_duration
   loop do
     loan_duration = gets.chomp
 
-    if valid_integer?(loan_duration)
+    if valid_duration?(loan_duration)
       loan_duration = loan_duration.to_i
       break
     else
-      prompt("Not a valid loan duration. Try again:")
+      display_invalid_duration
     end
   end
 
   form = (loan_amt * (monthly_rate / (1 - (1 + monthly_rate)**-loan_duration)))
   monthly_payment = form.round(2)
 
-  prompt("Processing information...")
-  prompt("Your monthly payment will be $#{monthly_payment}")
-  prompt("Do you want to calculate again? (Y to repeat, N to quit)")
-
+  display_monthly_payment(monthly_payment)
+  request_replay
   answer = gets.chomp
-  break if answer.downcase == "n"
-
-  valid_answer = ""
-  while answer.downcase != "y"
-    prompt("Enter Y to repeat, N to quit")
-    valid_answer = gets.chomp
-    break if valid_answer.downcase == "y" || valid_answer.downcase == "n"
-  end
-
-  break if valid_answer.downcase == "n"
+  break unless play_again?(answer)
 end
-prompt("----------------------------------------------------------------------")
-prompt("Thank you for using the car loan calculator. Hope you found it useful!")
+display_end
