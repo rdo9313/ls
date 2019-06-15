765de605b6dbcb5dd04e6086c1973cbc4940ca27
Ryan <rdo9313@gmail.com>
2019-06-15 12:59:41 -0400

    medium_2 exercises

--- exercises/small_problems/medium_2.rb
+++ exercises/small_problems/medium_2.rb
@@ -52,3 +52,89 @@ def letter_percentages(str)
   totals["neither"] = (neither.size / total_count) * 100
   totals
 end
+
+#4 Matching Parentheses?
+def balanced?(str)
+  parens = 0
+
+  str.chars.each do |char|
+    parens += 1 if char == "("
+    parens -= 1 if char == ")"
+    break if parens < 0
+  end
+  parens.zero?
+end
+
+#5 Triangle Sides
+def triangle(a, b, c)
+  sides = [a,b,c]
+  max = sides.max
+
+  if sides.include?(0) || max >= (sides.reduce(:+) - max)
+    :invalid
+  elsif a == b && b == c
+    :equilateral
+  elsif a == b || b == c || a == c
+    :isosceles
+  else
+    :scalene
+  end
+end
+
+#6 Tri-Angles
+def triangle(a, b, c)
+  angles = [a,b,c]
+
+  if angles.reduce(:+) != 180 || angles.include?(0)
+    :invalid
+  elsif angles.include?(90)
+    :right
+  elsif angles.any? { |angle| angle > 90 }
+    :obtuse
+  else
+    :acute
+  end
+end
+
+#7 Unlucky Days
+def friday_13th(yr)
+  month = 1
+  count = 0
+  loop do
+    count += 1 if Time.new(yr, month, 13).friday?
+    month += 1
+    break if month > 12
+  end
+  count
+end
+
+#8 Next Featured Number Higher than a Given Value
+def featured(num)
+  test = num + 1
+  loop do
+    if test.odd? && (test % 7 == 0) && test.to_s.chars.size == test.to_s.chars.uniq.size
+      break
+    else
+      test += 1
+    end
+  end
+  test
+end
+
+#9 Bubble Sort
+def bubble_sort!(array)
+  loop do
+    swapped = false
+    1.upto(array.size - 1) do |i|
+      next if array[i-1] <= array[i]
+      array[i-1], array[i] = array[i], array[i-1]
+      swapped = true
+    end
+    break unless swapped
+  end
+end
+
+#10 Sum Square - Square Sum
+def sum_square_difference(num)
+(1..num).reduce(:+) ** 2 - (1..num).reduce(0) { |sum, int| sum + int **2}
+end
