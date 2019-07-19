a0575cbce688d238758713c33cdb15e6e855f308
Ryan <rdo9313@gmail.com>
2019-07-19 11:56:09 -0400

    refactor design choice 2

--- rb120_oop/lesson_2/tictactoe.rb
+++ rb120_oop/lesson_2/tictactoe.rb
@@ -1,3 +1,48 @@
+require 'pry'
+class Move
+  VALUES = ['rock', 'paper', 'scissors']
+
+  def initialize(value)
+    @value = value
+  end
+
+  def scissors?
+    @value == 'scissors'
+  end
+
+  def rock?
+    @value == 'rock'
+  end
+
+  def paper?
+    @value == 'paper'
+  end
+
+  def >(other_move)
+    if rock?
+      other_move.scissors?
+    elsif paper?
+      other_move.rock?
+    elsif scissors?
+      other_move.paper?
+    end
+  end
+
+  def <(other_move)
+    if rock?
+     other_move.paper?
+    elsif paper?
+     other_move.scissors?
+    elsif scissors?
+     other_move.rock?
+    end
+  end
+
+  def to_s
+    @value
+  end
+end
+
 class RPSGame
   attr_accessor :human, :computer
   def initialize
@@ -17,19 +62,12 @@ class RPSGame
     puts "#{human.name} chose #{human.move}"
     puts "#{computer.name} chose #{computer.move}"
 
-    case human.move
-    when 'rock'
-      puts "It's a tie!" if computer.move == 'rock'
-      puts "#{human.name} won!" if computer.move == "scissors"
-      puts "#{computer.name} won!" if computer.move == "paper"
-    when 'paper'
-      puts "It's a tie!" if computer.move == 'paper'
-      puts "#{human.name} won!" if computer.move == "rock"
-      puts "#{computer.name} won!" if computer.move == "scissors"
-    when 'scissors'
-      puts "It's a tie!" if computer.move == 'scissors'
-      puts "#{human.name} won!" if computer.move == "paper"
-      puts "#{computer.name} won!" if computer.move == "rock"
+    if human.move > computer.move
+      puts "#{human.name} won!"
+    elsif human.move < computer.move
+      puts "#{computer.name} won!"
+    else
+      puts "It's a tie!"
     end
   end
 
@@ -80,10 +118,10 @@ class Human < Player
     loop do
       puts "Please choose rock, paper, or scissors:"
       choice = gets.chomp
-      break if ['rock', 'paper', 'scissors'].include?(choice)
+      break if Move::VALUES.include?(choice)
       puts "Sorry, invalid choice."
     end
-    self.move = choice
+    self.move = Move.new(choice)
   end
 end
 
@@ -93,7 +131,7 @@ class Computer < Player
   end
 
   def choose
-    self.move = ['rock', 'paper', 'scissors'].sample
+    self.move = Move.new(Move::VALUES.sample)
   end
 end
 
