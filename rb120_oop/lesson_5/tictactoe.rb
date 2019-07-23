304c6d7cc28e45e928df9264330248513d0aa1c4
Ryan <rdo9313@gmail.com>
2019-07-23 10:26:28 -0400

    defined classes/method skeleton based on nouns and verbs

---
+++ rb120_oop/lesson_5/tictactoe.rb
@@ -0,0 +1,28 @@
+class Board
+  def initialize
+  end
+end
+
+class Square
+  def initialize
+  end
+end
+
+class Player
+  def initialize
+  end
+
+  def mark
+  end
+
+  def play
+  end
+end
+
+class TTTGame
+  def play
+  end
+end
+
+game = TTTGame.new
+game.play
