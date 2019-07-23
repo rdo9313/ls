053e3cf194011f729091931c18b3ad7a1bf68a33
Ryan <rdo9313@gmail.com>
2019-07-23 11:22:16 -0400

    take turns/full board logic

--- rb120_oop/lesson_5/tictactoe.rb
+++ rb120_oop/lesson_5/tictactoe.rb
@@ -1,9 +1,8 @@
 require 'pry'
 class Board
-  INITIAL_MARKER = " "
   def initialize
     @squares = {}
-    (1..9).each { |key| @squares[key] = Square.new(INITIAL_MARKER)}
+    (1..9).each { |key| @squares[key] = Square.new}
   end
 
   def get_square_at(key)
@@ -13,16 +12,29 @@ class Board
   def set_square_at(key, marker)
     @squares[key].marker = marker
   end
+
+  def unmarked_keys
+    @squares.keys.select { |key| @squares[key].unmarked? }
+  end
+
+  def full?
+    unmarked_keys.empty?
+  end
 end
 
 class Square
+  INITIAL_MARKER = " "
   attr_accessor :marker
-  def initialize(marker)
+  def initialize(marker = INITIAL_MARKER)
     @marker = marker
   end
 
   def to_s
     @marker
+  end
+
+  def unmarked?
+    marker == INITIAL_MARKER
   end
 end
 
@@ -56,6 +68,8 @@ class TTTGame
   end
 
   def display_board
+    system("clear")
+    puts "You're a #{human.marker}. Computer is a #{computer.marker}."
     puts ""
     puts "     |     |"
     puts "  #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |  #{board.get_square_at(3)}"
@@ -72,11 +86,11 @@ class TTTGame
   end
 
   def human_moves
-    puts "Choose a square between 1-9: "
+    puts "Choose a square between #{board.unmarked_keys.join(', ')}: "
     square = nil
     loop do
       square = gets.chomp.to_i
-      break if (1..9).include?(square)
+      break if board.unmarked_keys.include?(square)
       puts "Sorry, that's not a valid choice."
     end
 
@@ -84,21 +98,28 @@ class TTTGame
   end
 
   def computer_moves
-    board.set_square_at((1..9).to_a.sample, computer.marker)
+    board.set_square_at(board.unmarked_keys.sample, computer.marker)
+  end
+
+  def display_result
+    display_board
+    puts "The board is full!"
   end
 
   def play
     display_welcome_message
-    loop do
-      display_board
-      human_moves
-      display_board
-      break if soemone_won? || board_full?
+    display_board
 
+    loop do
+      human_moves
+      break if board.full?
+      # break if soemone_won? || board_full?
       computer_moves
-      break if soemone_won? || board_full?
+      break if board.full?
+      display_board
+      # break if soemone_won? || board_full?
     end
-    # display_result
+    display_result
     display_goodbye_message
   end
 end
