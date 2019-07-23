b930cb67e64f3fd1d3caf50de2ca078bdff9a25f
Ryan <rdo9313@gmail.com>
2019-07-23 11:06:29 -0400

    human/computer moves logic

--- rb120_oop/lesson_5/tictactoe.rb
+++ rb120_oop/lesson_5/tictactoe.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Board
   INITIAL_MARKER = " "
   def initialize
@@ -8,9 +9,14 @@ class Board
   def get_square_at(key)
     @squares[key]
   end
+
+  def set_square_at(key, marker)
+    @squares[key].marker = marker
+  end
 end
 
 class Square
+  attr_accessor :marker
   def initialize(marker)
     @marker = marker
   end
@@ -21,10 +27,9 @@ class Square
 end
 
 class Player
-  def initialize
-  end
-
-  def mark
+  attr_reader :marker
+  def initialize(marker)
+    @marker = marker
   end
 
   def play
@@ -32,9 +37,13 @@ class Player
 end
 
 class TTTGame
-  attr_reader :board
+  HUMAN_MARKER = "X"
+  COMPUTER_MARKER = "O"
+  attr_reader :board, :human, :computer
   def initialize
     @board = Board.new
+    @human = Player.new(HUMAN_MARKER)
+    @computer = Player.new(COMPUTER_MARKER)
   end
 
   def display_welcome_message
@@ -62,15 +71,31 @@ class TTTGame
     puts ""
   end
 
+  def human_moves
+    puts "Choose a square between 1-9: "
+    square = nil
+    loop do
+      square = gets.chomp.to_i
+      break if (1..9).include?(square)
+      puts "Sorry, that's not a valid choice."
+    end
+
+    board.set_square_at(square, human.marker)
+  end
+
+  def computer_moves
+    board.set_square_at((1..9).to_a.sample, computer.marker)
+  end
+
   def play
     display_welcome_message
     loop do
       display_board
-      break
-      first_player_moves
+      human_moves
+      display_board
       break if soemone_won? || board_full?
 
-      second_player_moves
+      computer_moves
       break if soemone_won? || board_full?
     end
     # display_result
