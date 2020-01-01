c17bf648d2df5a100b50373886e234ef6b462410
Ryan <rdo9313@gmail.com>
2020-01-01 09:48:19 -0500

    the basics exercises

---
+++ prep_js/the_basics/exercises.js
@@ -0,0 +1,41 @@
+1) "John " + "Doe"
+
+2) let number = 4936
+let ones = number % 10  //6
+number = (number - ones) / 10
+let tens = number % 10  //3
+number = (number - tens) / 10
+let hundreds = number % 10 //9
+let thousands = (number - hundreds) / 10 //4
+
+3) 'true' => string
+false => boolean
+1.5 => number
+2 => number
+undefined => undefined
+{ foo: 'bar' } => object
+
+4) var foo => statement
+foo = 5 => expression
+foo => expression
+
+5) The integer 10 is coerced into a string and concatenated to the string 5.
+
+6) console.log(Number('5') + 10);
+
+7) `The value of ${Number('5')} + 10 is 15.`
+
+8) returns undefined
+
+9) let names = ['asta', 'butterscotch', 'pudding', 'neptune', 'darwin'];
+
+10) let pets = { asta: 'dog', butterscotch: 'cat', pudding: 'cat', neptune: 'fish', darwin: 'lizard' };
+
+11) false
+
+12) 3
+
+13) true
+
+
+
