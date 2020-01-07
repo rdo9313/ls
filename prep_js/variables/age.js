fcf66edd150aef8f5c3f737c0a1ed5728414d5b0
Ryan <rdo9313@gmail.com>
2020-01-06 19:38:53 -0500

    loops exercises

--- prep_js/variables/age.js
+++ prep_js/variables/age.js
@@ -1,7 +1,6 @@
 let readlineSync = require('readline-sync');
 let age = Number(readlineSync.question("How old are you? "));
 console.log(`You are ${age} years old.`);
-console.log(`In 10 years, you will be ${age + 10} years old.`);
-console.log(`In 10 years, you will be ${age + 20} years old.`);
-console.log(`In 10 years, you will be ${age + 30} years old.`);
-console.log(`In 10 years, you will be ${age + 40} years old.`);
+for(let future = 10; future < 50; future += 10) {
+  console.log(`In ${future} years, you will be ${age + future} years old.`);
+}
