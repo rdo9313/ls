1b6e79afe1912e8681ee6e9223c9f58283b97e70
Ryan <rdo9313@gmail.com>
2020-01-09 19:47:28 -0500

    more stuff exercises

---
+++ prep_js/more_stuff/exercises.js
@@ -0,0 +1,26 @@
+#1
+Logs [1,4,3] because the assignment is destructive.
+
+#2
+the variable 'greeting' is undefined within the current execution context.
+
+#3
+console.log(Math.sqrt(37))
+
+#4
+Math.max(1,6,3,2)
+Math.max(-1,-6,-3,-2)
+Math.max(2,2)
+
+#5
+Splits the string by space, reverses the resulting array, and then returns a new array with the character lengths 
+per element (string) in the array.
+
+#6
+function logMatch(words, regex) {
+  return words.filter(word => regex.test(word))
+}
+
+#7
+Exception handling is dealing with errors in a manageable and predictable manner. 
+Javascript uses try/catch to catch exceptions in the code.
