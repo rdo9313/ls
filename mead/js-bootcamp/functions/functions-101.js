a9d442b5cf60d498dfa8877a191ebcee654af208
Ryan <rdo9313@gmail.com>
2020-01-15 16:16:06 -0500

    j100_functions2 exercises

--- mead/js-bootcamp/functions/functions-101.js
+++
@@ -1,7 +0,0 @@
-let convertFarenheitToCelsius = function (farenheit) {
-  let celsius = (farenheit - 32) * 5 / 9;
-  return celsius;
-}
-
-console.log(convertFarenheitToCelsius(32));
-console.log(convertFarenheitToCelsius(68));
--- prep_js/variables/age.js
+++
@@ -1,6 +0,0 @@
-let readlineSync = require('readline-sync');
-let age = Number(readlineSync.question("How old are you? "));
-console.log(`You are ${age} years old.`);
-for(let future = 10; future < 50; future += 10) {
-  console.log(`In ${future} years, you will be ${age + future} years old.`);
-}
