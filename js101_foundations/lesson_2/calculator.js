fb5f911b8b65549dfabec18169021ceaad331589
Ryan <rdo9313@gmail.com>
2020-02-18 01:28:56 -0500

    calculator bonus features

--- js101_foundations/lesson_2/calculator.js
+++ js101_foundations/lesson_2/calculator.js
@@ -1,24 +1,73 @@
 const readline = require('readline-sync');
+const MESSAGES = require('./calculator_messages.json');
 
-console.log('Welcome to Calculator!');
-
-console.log("What's the first number?");
-let number1 = readline.question();
-
-console.log("What's the second number?");
-let number2 = readline.question();
-
-console.log("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide");
-let operation = readline.question();
-
-let output;
-if (operation === '1') {
-  output = Number(number1) + Number(number2);
-} else if (operation === '2') {
-  output = Number(number1) - Number(number2);
-} else if (operation === '3') {
-  output = Number(number1) * Number(number2);
-} else if (operation === '4') {
-  output = Number(number1) / Number(number2);
+function prompt(message) {
+  console.log(`=> ${message}`);
 }
-console.log(`The result is: ${output}`);
+
+function messages(message, lang = 'en') {
+  return MESSAGES[lang][message];
+}
+
+function invalidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
+
+while (true) {
+  console.clear()
+  prompt(messages('greeting'));
+
+  prompt(messages('first_num'));
+  let number1 = readline.question();
+
+  while (invalidNumber(number1)) {
+    prompt(messages('invalid_input'));
+    number1 = readline.question();
+  }
+
+  prompt(messages('second_num'));
+  let number2 = readline.question();
+
+  while (invalidNumber(number2)) {
+    prompt(messages('invalid_input'));
+    number2 = readline.question();
+  }
+
+  prompt(messages('operation'));
+  let operation = readline.question();
+
+  while (!['1','2','3','4'].includes(operation)) {
+    prompt(messages('user_choice'));
+    operation = readline.question();
+  }
+
+  let output;
+  switch (operation) {
+    case '1':
+      output = Number(number1) + Number(number2);
+      break;
+    case '2':
+      output = Number(number1) - Number(number2);
+      break;
+    case '3':
+      output = Number(number1) * Number(number2);
+      break;
+    case '4':
+      output = Number(number1) / Number(number2);
+      break;
+  }
+
+  prompt(`The result is: ${output}`);
+
+  prompt(messages('again'));
+  let again = readline.question();
+
+  while (!['y','yes','n','no'].includes(again.toLowerCase())) {
+    prompt(messages('valid'));
+    again = readline.question();
+  }
+
+  if (['n', 'no'].includes(again.toLowerCase())) break;
+}
+
+prompt(messages("goodbye"));
