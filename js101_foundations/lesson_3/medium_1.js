21683f5e79a6e3256dbfb20989fe182b0bb5cc0b
Ryan <rdo9313@gmail.com>
2020-02-23 11:50:25 -0500

    medium_1 exercises

---
+++ js101_foundations/lesson_3/medium_1.js
@@ -0,0 +1,47 @@
+// 1
+const flintstones = "The Flintstones Rock!";
+for(let i = 0; i < 10; i++) {
+  console.log(flintstones.padStart(flintstones.length + i));
+}
+
+// 2
+let arr = [];
+for (char of munstersDescription) {
+  if (char.toUpperCase() === char) {
+    arr.push(char.toLowerCase());
+  } else {
+    arr.push(char.toUpperCase());
+  }
+}
+arr.join('')
+
+// 3
+while (divisor > 0) {
+  if (number % divisor === 0) {
+    factors.push(number / divisor);
+  }
+  divisor -= 1;
+}
+
+// 4
+The first method mutates 'buffer' while the second does not.
+
+// 5
+0.899999999
+false
+
+// 6
+false
+isNaN()
+
+// 7
+34
+
+// 8
+Yes data was mutated because the object is passed by reference.
+
+// 9
+'paper'
+
+// 10
+"no"
