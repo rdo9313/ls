3a3f08f3552b093e216ffb9d82e58522ae79430c
Ryan <rdo9313@gmail.com>
2020-03-02 21:02:20 -0500

    lesson_5 problems

---
+++ js101_foundations/lesson_5/problems.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+// 1
+arr.sort((a,b) => Number(b) - Number(a));
+
+// 2
+books.sort((a,b) => Number(a.published) - Number(b.published));
+
+// 3
+arr1[2][1][3];
+arr2[1]["third"][0];
+arr3[2]["third"][0][0];
+obj1.b[1];
+Object.keys(obj2.third)[0];
+
+// 4
+arr1[1][1] = 4;
+arr2[2] = 4;
+obj1.first[2][0] = 4;
+obj2.a.a[2] = 4;
+
+// 5
